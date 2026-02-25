fee14f6370d74bf1f01446eaecd61d3c3ae276b3
Choa0Kim <asjklcom@gmail.com>
2026-02-25 21:26:24 +0900

    upload 0225-2

--- study_02_25/백준/블랙잭.py
+++ study_02_25/백준/블랙잭.py
@@ -4,20 +4,37 @@ sys.stdin = open("input.txt", "r")
 N, M = map(int, input().split())
 numbers = list(map(int, input().split()))
 # print(numbers)
+flg = True
 total = 0
+max_total = []
+
 # 첫번째 카드 설정
 for i in range(N):
   total += numbers[i]
-  card1= numbers[i]
+  
   # 두번째 카드 설정
-  for j in range(1, N):
+  for j in range(i+1, N):
     total += numbers[j]
-    card2 = numbers[j]
+   
     # 세번째 카드 설정
-    for k in range(2, N):
+    for k in range(j+1, N):
       total += numbers[k]
-      card3 = numbers[k]
+     
+      # 다 돌았는데, total이 M보다 크면 break
+      if total > M:
+        total -= numbers[k]
+        # print(total)
+        continue  
+        
+      # 아니라면 total을 max_total list에 넣기
+      else:
+        max_total.append(total)    
+        total -= numbers[k]
+  
+    total -=numbers[j]
 
-      # total의 값을 M과 비교
-      if total <
+  total -= numbers[i]  
       
+   
+print(max(max_total))
+
---
+++ study_02_25/백준/블랙잭2.py
@@ -0,0 +1,40 @@
+import sys
+sys.stdin = open("input.txt", "r")
+
+
+N, M = map(int, input().split())
+numbers = list(map(int, input().split()))
+# print(numbers)
+
+total = 0
+max_total = 0
+
+def recur(cnt, idx):
+    global total
+    global max_total
+
+    # 종료조건: 카드를 3장 뽑았을떄
+    if cnt == 3 :
+        # 그 카드들의 합이 21 이하일때, 최댓갑을 갱신
+        if M >= total:
+            max_total = max(max_total, total)
+
+        # if M >= total > max_total:
+        #     max_total = total
+
+        return
+
+
+    # 반복할 동작
+    for i in range(idx, N):
+        total += numbers[i]
+
+        recur(cnt+1, i+1)
+        # 종료조건을 만나서 다음턴으로 갈 작업
+        total -= numbers[i]   
+
+
+# 초기 입력값
+recur(0, 0)
+
+print(max_total)
